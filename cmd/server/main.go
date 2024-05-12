2daf2e03d9c80d3e209f203beacdf6ff9568327f
Андрей Белов <dry.wats@gmail.com>
2024-05-12 22:26:40 +0300

    Configured the base storage and migrations in the code

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,12 +6,28 @@ import (
 	"github.com/MWT-proger/go-keeper/configs/server/config"
 	"github.com/MWT-proger/go-keeper/internal/pkg/logger"
 	"github.com/MWT-proger/go-keeper/internal/server/handlers"
+	"github.com/MWT-proger/go-keeper/internal/server/models"
 	"github.com/MWT-proger/go-keeper/internal/server/rest"
 	"github.com/MWT-proger/go-keeper/internal/server/services"
 	"github.com/MWT-proger/go-keeper/internal/server/store"
 )
 
-var storage store.UserStore
+type UserStore struct {
+	*store.Store
+}
+
+func NewUserStore(baseStorage *store.Store) (*UserStore, error) {
+
+	return &UserStore{baseStorage}, nil
+}
+
+func (s *UserStore) Insert(ctx context.Context, obj *models.User) error {
+	return nil
+}
+func (s *UserStore) GetFirstByParameters(ctx context.Context, args map[string]interface{}) (*models.User, error) {
+	user, _ := models.NewUser()
+	return user, nil
+}
 
 func main() {
 
@@ -27,21 +43,25 @@ func main() {
 // run() выполняет все предворительные действия и вызывает функцию запуска сервера
 func run(ctx context.Context) error {
 	// Просто заглушка для контекста пока
-	_, cancel := context.WithCancel(ctx)
+	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 	var conf = config.InitConfig()
 
 	if err := logger.Initialize(conf.LogLevel); err != nil {
 		return err
 	}
+	storage, err := store.NewStore(ctx, conf)
+	if err != nil {
+		return err
+	}
 
-	storage, err := store.NewUserStore()
+	userStorage, err := NewUserStore(storage)
 
 	if err != nil {
 		return err
 	}
 
-	userService := services.NewUserService(storage, conf)
+	userService := services.NewUserService(userStorage, conf)
 
 	h, err := handlers.NewAPIHandler(userService)
 
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/MWT-proger/go-keeper
 go 1.21.6
 
 require (
-	github.com/MWT-proger/go-loyalty-system v0.0.0-20240105113649-aebed4d9e763
 	github.com/gofrs/uuid v4.4.0+incompatible
 	github.com/golang-jwt/jwt/v4 v4.5.0
 	go.uber.org/zap v1.27.0
@@ -11,7 +10,19 @@ require (
 )
 
 require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/sethvargo/go-retry v0.2.4 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+)
+
+require (
 	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/cors v1.2.1
-	go.uber.org/multierr v1.10.0 // indirect
+	github.com/jackc/pgx/v5 v5.5.5
+	github.com/pressly/goose/v3 v3.20.0
+	go.uber.org/multierr v1.11.0 // indirect
 )
